8e21d4a3af100a259469db1337cf342208ecee7e
BrianMejia-creator <69980529+BrianMejia-creator@users.noreply.github.com>
2020-08-20 12:20:46 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/*
+*Hello Java
+*Brian Mejia
+*08/20/2020
+*CMAC255-01
+*/
+public class Hello {
+	public static void main(String[]args) {
+	System.out.println("<BrianMejia>");
+	System.out.println("<Introduction into Java>");
+	System.out.println("<for this labratory we will be downloading Java apps and testing it out as well on terminal>");
+	System.out.println("<August 20 2020>");
+	System.out.println("<CMSC 255 001>");
+	System.out.println();
+	}
+}
